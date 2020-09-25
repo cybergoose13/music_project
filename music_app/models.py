ffd5ffa14aa68fd57c3de2266d30306979108e6f
Allen-Valdez <68507863+Allen-Valdez@users.noreply.github.com>
2020-09-24 23:26:48 -0700

    views py

--- music_app/models.py
+++ music_app/models.py
@@ -24,52 +24,44 @@ class RegisterManager(models.Manager):
             EMAIL_REGEX= re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
             PASS_REGEXT= 'r^"(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$])[\w\d!@#$]{8,}$"'
 
-            if postLenCheck(userName):
-                if len(User.objects.filter(user_name= userName)) > 0:
-                    print('user name is taken. . .')
-                    errors['USER_NAME_EXISTS_ERROR']= 'User name is taken.'
-                if len(userName) < 5:
-                    print('user name is less than 5 charaters. . .')
-                    errors['USER_NAME_LEN_ERROR']= 'User name must be atleast 5 characters.'
-            if postLenCheck(userEmail):
-                if len(User.objects.filter(user_email= userEmail)) > 0:
-                    print('email is already in use. . .')
-                    errors['USER_EMAIL_EXISTS_ERROR']= 'Invalid email.'
-                if not EMAIL_REGEX.match(userEmail):
-                    print('user email syntax error. . .')
-                    errors['USER_EMAIL_FORMAT_ERROR']= 'Invalid email.'
-            if postLenCheck(userPass):
-                if not re.match(PASS_REGEXT, userPass):
-                    print('password syntax error. . .')
-                    errors['PASSWORD_FORM_ERROR']= 'Password must have atleast 1 lowercase 1 uppercase and special character.'
-                if userPass != passConfirm:
-                    print('passwords do not match error. . .')
-                    errors['PASSWORD_MATCH_ERROR']= 'Passwords do not match.'
-            if postLenCheck(firstName) and len(firstName) < 5:
+            
+            if len(User.objects.filter(user_name= userName)) > 0:
+                print('user name is taken. . .')
+                errors['USER_NAME_EXISTS_ERROR']= 'User name is taken.'
+            if len(userName) < 5:
+                print('user name is less than 5 charaters. . .')
+                errors['USER_NAME_LEN_ERROR']= 'User name must be atleast 5 characters.'
+            if len(User.objects.filter(user_email= userEmail)) > 0:
+                print('email is already in use. . .')
+                errors['USER_EMAIL_EXISTS_ERROR']= 'Invalid email.'
+            if not EMAIL_REGEX.match(userEmail):
+                print('user email syntax error. . .')
+                errors['USER_EMAIL_FORMAT_ERROR']= 'Invalid email.'
+            
+            if not re.match(PASS_REGEXT, userPass):
+                print('password syntax error. . .')
+                errors['PASSWORD_FORM_ERROR']= 'Password must have atleast 1 lowercase 1 uppercase and special character.'
+            if userPass != passConfirm:
+                print('passwords do not match error. . .')
+                errors['PASSWORD_MATCH_ERROR']= 'Passwords do not match.'
+            if len(firstName) < 5:
                 print('first name is null or less than 5 char. . .')
                 errors['FIRST_NAME_ERROR']= 'First name must have atleast 5 characters.'
-            if postLenCheck(lastName) and len(lastName) < 5:
+            if len(lastName) < 5:
                 print('last name is null or less than 5 char. . .')
                 errors['LAST_NAME_ERROR']= 'Last name must have atleast 5 characters.'
 
-            return errors
+        return errors
 
     def loginValidation(self, postData):
         errors= {}
-        if postLenCheck(postData):
-            if postLenCheck(User.objects.filter(user_email= postData['user_email'])):
-                print('email not found. . .')
-                errors['LOGIN_ERROR']= 'Invalid login credentials.'
-            if not bcrypt.checkpw(postData['user_pass'].encode(), User.objects.get(user_email= postData['user_email']).password.encode()):
-                print('password did not match')
-                errors['LOGIN_ERROR']= 'Invalid login credentials.'
-            return errors
-
-    def postLenCheck(self, key):
-        if len(key) > 0:
-            return True
-        else:
-            return False
+        if len(User.objects.filter(user_email= postData['user_email'])) > 5:
+            print('email not found. . .')
+            errors['LOGIN_ERROR']= 'Invalid login credentials.'
+        if not bcrypt.checkpw(postData['user_pass'].encode(), User.objects.get(user_email= postData['user_email']).password.encode()):
+            print('password did not match')
+            errors['LOGIN_ERROR']= 'Invalid login credentials.'
+        return errors
 
 class Post(models.Model):
     post= models.CharField(max_length= 255)
