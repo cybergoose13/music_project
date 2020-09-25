b63215cfbdf3d7af4f68f359d59a3fb935e94253
BuildTools <unconfigured@null.spigotmc.org>
2020-09-25 01:42:48 -0700

    models update

--- music_app/models.py
+++ music_app/models.py
@@ -65,6 +65,9 @@ class RegisterManager(models.Manager):
 
 class Post(models.Model):
     post_text = models.CharField(max_length= 255)
+    artist_name= models.CharField(max_length= 255)
+    song_name= models.CharField(max_length= 255)
+    album_pic= models.CharField(max_length= 255)
     posted_by = models.ForeignKey('User', related_name ='post', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now= True)
