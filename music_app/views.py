c0ee5e7909101542635e6e167f708e24dff8e2e0
Allen-Valdez <68507863+Allen-Valdez@users.noreply.github.com>
2020-09-24 19:15:55 -0700

    suck my ass

--- music_app/views.py
+++ music_app/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
+from .models import *
 
 # login should be the landing page of localhost:8000/
-# 
-def login(request):
-    context={
+# The following URLS are for RENDER
+def index(request):
+    return render(request, 'login.html')
 
+def dashboard(request):
+    if 'uuid' not in request.session:
+        print("No session in progress, you failed the vibe check.")
+        return redirect('/')
+    
+    this_user = User.objects.get(id=request.session['uuid'])
+    every_users_post = Post.objects.all()
+    this_users_post = this_user.post.all()
+    
+    print("You are in the dashboard! Let's start listening!")
+    context = {
+        'user_object' : this_user,
+        'every_users_post' : every_users_post,
     }
-    return render(request, 'login.html', context)
+    
+    return render(request,'dashboard.html', context)
+
+#########################################################
+
+#This is the end of the URLS for RENDER
+def register(request):
+    print("Register function", request.POST)
+    errors = User.objects.
+
+#The following URLS are for REDIRECTING
+
+
+
+
+
+
+
+
+
+# This is the end of the URLS for REDIRECTING
